686b0896123f9148e01a687c197e16601050375f
Marie89 <marie.lanfermann@gmail.com>
2015-11-05 15:56:49 +0100

    basic construct

---
+++ envsetup-athena.sh
@@ -0,0 +1,3 @@
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+export ALRB_localConfigDir=$HOME/localConfig
---
+++ setup_TestArea_public.sh
@@ -0,0 +1,3 @@
+source envsetup-athena.sh 
+cd public/TestArea_20.1.6.3/
+asetup 20.1.6.3,AtlasDerivation,gcc48,here
