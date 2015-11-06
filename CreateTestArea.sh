af48026630f505d5ed1f62a78c3d6c5a48e74c48
Marie89 <marie.lanfermann@gmail.com>
2015-11-06 18:41:38 +0100

    branch update
    Included checkout of branches responsible for DL1 integration

--- CreateTestArea.sh
+++ CreateTestArea.sh
@@ -1,11 +1,12 @@
+source envsetup-athena.sh 
 mkdir TestArea_20.1.6.3_Oct23
 cd TestArea_20.1.6.3_Oct23
 asetup 20.1.6.3,AtlasDerivation,gcc48,here
+pkgco.py BTagging-00-07-43-branch
+pkgco.py JetTagTools-01-00-62-branch
 pkgco.py JetInterface-00-00-43
 pkgco.py JetMomentTools-00-03-20
 pkgco.py PileupReweighting-00-03-06
-pkgco.py JetTagTools-01-00-56
-pkgco.py BTagging-00-07-43
 svn co svn+ssh://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/FlavourTagPerformanceFramework/trunk/xAODAthena xAODAthena
 setupWorkArea.py
 cd WorkArea/cmt
