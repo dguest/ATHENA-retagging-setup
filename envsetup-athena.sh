9b0f6b366d15c017f7784a0d1ac6a4cae3f38bd7
Marie89 <Marie89@users.noreply.github.com>
2015-11-08 16:24:06 +0100

    Delete envsetup-athena.sh
    no longer needed (included in other scripts)

--- envsetup-athena.sh
+++
@@ -1,3 +0,0 @@
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-export ALRB_localConfigDir=$HOME/localConfig
