06c7d90bc8bf8724930919b58a23e5902acfec9b
Marie Lanfermann <marie.lanfermann@cern.ch>
2016-02-03 13:56:19 +0100

    replaced patch by complete job options

---
+++ jobOptions_Tag.py
@@ -0,0 +1,206 @@
+##########################################################################################################################################################
+##########################################################################################################################################################
+### MAIN SWITCHES
+
+ReduceInfo        =False   ## write minimal amount of info on the output file
+DoMSV             =False   ## include variables for MSV tagger
+doSMT             =False   ## include variables for SMT tagger
+doRetag           =True    ## perform retagging
+doComputeReference=False
+JetCollections = [
+  ##"AntiKt10LCTopoJets"
+  'AntiKt4EMTopoJets', 
+  #'AntiKt3PV0TrackJets',
+  #'AntiKt2PV0TrackJets',
+  #'AntiKt4LCTopoJets', 
+  ]
+
+
+#########################################################################################################################################################
+#########################################################################################################################################################
+### Define input xAOD and output ntuple file name
+import glob
+from AthenaCommon.AthenaCommonFlags import jobproperties as jp
+jp.AthenaCommonFlags.EvtMax.set_Value_and_Lock(10)
+
+jp.AthenaCommonFlags.FilesInput = [ 
+  "/afs/cern.ch/work/m/malanfer/public/mc15/AOD.05419191._000184.pool.root.1"
+  ##"/tmp/vdao/mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.AOD.e3698_s2608_s2183_r6630_r6264_tid05419257_00/AOD.05419257._000001.pool.root.1"
+  #"/afs/cern.ch/user/n/nwhallon/work/public/xAOD_samples/mc15_13TeV.361021.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ1W.merge.AOD.e3569_s2576_s2132_r6630_r6264_tid05403632_00/AOD.05403632._000098.pool.root.1"
+  ]
+svcMgr += CfgMgr.THistSvc()
+for jet in JetCollections:
+  shortJetName=jet.replace("AntiKt","Akt").replace("TopoJets","To").replace("TrackJets","Tr")
+  svcMgr.THistSvc.Output += [ shortJetName+" DATAFILE='flav_"+shortJetName+".root' OPT='RECREATE'"]
+#svcMgr.THistSvc.Output += ["BTAGSTREAM DATAFILE='flavntuple.root' OPT='RECREATE'"]
+
+
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+### you should normally not need to touch this part
+
+doRecomputePV=False  ## do not touch unless you know what you are doing
+if doSMT: doRetag=True
+if doComputeReference:
+  ReduceInfo   =True
+  doRetag      =True
+  doRecomputePV=False
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+### VD: put first all the RecExCommon things, then the B-tagging stuff
+if doRecomputePV:
+  from InDetRecExample.InDetJobProperties import InDetFlags
+  InDetFlags.doVertexFinding.set_Value_and_Lock(True)
+  from InDetRecExample.InDetKeys import InDetKeys
+  InDetKeys.xAODVertexContainer.StoredValue='PrimaryVerticesValerio'
+
+## from Anthony: needed to compute truth quantities of tracks
+from AthenaCommon.DetFlags import DetFlags
+DetFlags.BField_setOn()
+DetFlags.ID_setOn()
+DetFlags.Calo_setOff()
+DetFlags.Muon_setOff()
+if doSMT: DetFlags.Muon_setOn()
+
+from RecExConfig.RecFlags import rec
+rec.doTrigger.set_Value_and_Lock(True)
+from RecExConfig.RecAlgsFlags  import recAlgs
+recAlgs.doTrigger.set_Value_and_Lock(True)
+from TriggerJobOpts.TriggerFlags import TriggerFlags
+TriggerFlags.doTriggerConfigOnly.set_Value_and_Lock(True)
+
+from RecExConfig.RecFlags import rec
+rec.doESD.set_Value_and_Lock        (False)
+rec.doWriteESD.set_Value_and_Lock   (False)
+rec.doAOD.set_Value_and_Lock        (False)
+rec.doWriteAOD.set_Value_and_Lock   (False)
+rec.doWriteTAG.set_Value_and_Lock   (False)
+rec.doDPD.set_Value_and_Lock        (False)
+rec.doTruth.set_Value_and_Lock      (False)
+
+#############rec.doApplyAODFix.set_Value_and_Lock(False)
+
+include ("RecExCommon/RecExCommon_topOptions.py")
+
+from AthenaCommon.AlgSequence import AlgSequence
+algSeq = AlgSequence()
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+### GEO Business 
+from AthenaCommon.GlobalFlags import globalflags
+print "detDescr from global flags= "+str(globalflags.DetDescrVersion)
+from AtlasGeoModel.InDetGMJobProperties import GeometryFlags as geoFlags
+print "geoFlags.Run()   = "+geoFlags.Run()
+print "geoFlags.isIBL() = "+str(  geoFlags.isIBL() )
+
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+### VD: this is if you want to re-tag with another calibration file
+from BTagging.BTaggingFlags import BTaggingFlags
+
+#### if the new file is already in the datatbase: simple edit the name
+#BTaggingFlags.CalibrationTag = 'BTagCalibRUN12-08-07'
+
+#### if you want to use your own calibration file use this part below
+BTaggingFlags.CalibrationFromLocalReplica = True
+BTaggingFlags.CalibrationFolderRoot = '/GLOBAL/BTagCalib/'
+BTaggingFlags.CalibrationTag = 'BTagCalibRUN2-test'
+#BTaggingFlags.CalibrationTag = 'Run2DC14' ## '0801C' ##'k0002'
+
+BTaggingFlags.DL1 = True
+# BTaggingFlags.DL1LocalNNconfig = "AGILEPack_b-tagging.weights.json"
+
+include("RetagFragment.py")
+
+
+if doRetag:
+  from BTagging.BTaggingConfiguration import getConfiguration
+  BTagConf = getConfiguration()
+
+  ip3d =BTagConf.getTool("IP3DTag", "BTagTrackToJetAssociator","AntiKt4EMTopo")
+  #ip3d.OutputLevel=DEBUG
+  #####ip3d.SortingMode="SortZ0D0"
+  
+  ip2d =BTagConf.getTool("IP2DTag", "BTagTrackToJetAssociator","AntiKt4EMTopo")
+  #####ip2d.SortingMode="SortPt"
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
+### Additional Tools needed by the dumper
+from TrackVertexAssociationTool.TrackVertexAssociationToolConf import CP__TightTrackVertexAssociationTool
+ToolSvc+=CP__TightTrackVertexAssociationTool("TightVertexAssocTool",dzSinTheta_cut=3, doPV=True)
+
+from InDetTrackSelectionTool.InDetTrackSelectionToolConf import InDet__InDetTrackSelectionTool
+ToolSvc+=InDet__InDetTrackSelectionTool("InDetTrackSelTool", CutLevel="Loose", maxZ0SinTheta=3)
+
+from TrkVertexFitterUtils.TrkVertexFitterUtilsConf import Trk__TrackToVertexIPEstimator
+ToolSvc+=Trk__TrackToVertexIPEstimator("trkIPEstimator")
+
+from TrigDecisionTool.TrigDecisionToolConf import Trig__TrigDecisionTool
+ToolSvc += Trig__TrigDecisionTool( "TrigDecisionTool" )
+from TrigEDMConfig.TriggerEDM import EDMLibraries
+ToolSvc.TrigDecisionTool.Navigation.Dlls = EDMLibraries
+
+jvt = CfgMgr.JetVertexTaggerTool('JVT')
+ToolSvc += jvt
+
+ToolSvc += CfgMgr.CP__PileupReweightingTool("prw",
+                                            OutputLevel = INFO,
+                                            UsePeriodConfig= "MC15"
+                                            )
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
+### Main Ntuple Dumper Algorithm
+for JetCollection in JetCollections:
+  shortJetName=JetCollection.replace("AntiKt","Akt").replace("TopoJets","To").replace("TrackJets","Tr")
+  alg = CfgMgr.btagIBLAnalysisAlg("BTagDumpAlg_"+JetCollection, 
+                                  OutputLevel=INFO,
+                                  Stream=shortJetName,
+                                  InDetTrackSelectionTool   =ToolSvc.InDetTrackSelTool,
+                                  TrackVertexAssociationTool=ToolSvc.TightVertexAssocTool,
+                                  TrackToVertexIPEstimator  =ToolSvc.trkIPEstimator,
+                                  JVTtool=ToolSvc.JVT,
+                                  ) #DEBUG
+  alg.JetCollectionName = JetCollection
+  alg.doSMT = doSMT
+  if "Track" in JetCollection or "Truth" in JetCollection:
+    alg.JetPtCut = 5.e3
+    alg.CleanJets = False
+    alg.CalibrateJets = False
+  else:
+    alg.JetPtCut = 20.e3
+  alg.doSMT     =doSMT
+  alg.ReduceInfo=ReduceInfo
+  alg.DoMSV     =DoMSV
+  alg.Rel20     =True
+  alg.JetCleaningTool.CutLevel= "LooseBad" 
+  alg.JetCleaningTool.DoUgly  = True
+  if not doComputeReference: algSeq += alg
+  
+  ###print JetCollection
+  calibfile        = "JES_Prerecommendation2015_Feb2015.config"
+  collectionForTool="AntiKt4LCTopo"
+  calSeg           ="JetArea_Residual_EtaJES"
+  if "EM" in JetCollection: 
+    collectionForTool="AntiKt4EMTopo"
+    calibfile  ="JES_MC15Prerecommendation_April2015.config"
+    #########################calibfile  ="JES_Prerecommendation2015_AFII_Apr2015.config"
+    calSeg     ="JetArea_Residual_Origin_EtaJES_GSC"
+  print collectionForTool
+  ToolSvc += CfgMgr.JetCalibrationTool("BTagDumpAlg_"+JetCollection+"_JCalib",
+                                       IsData=False,
+                                       ConfigFile=calibfile,
+                                       CalibSequence=calSeg,
+                                       JetCollection=collectionForTool) 
+
+
+###########################################################################################################################################################################
