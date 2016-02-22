710caf1553bbbd4868ec36f9bf5a9fe4e82828c5
Marie89 <marie.lanfermann@cern.ch>
2016-02-22 13:50:56 +0100

    added option to use the trunk

--- create_TestArea.sh
+++ create_TestArea.sh
@@ -1,4 +1,27 @@
+#!/usr/bin/env bash
 
+set -eu
+# ________________________________________________________________________
+function _usage() {
+    echo "usage $0 $1 <branch or trunk>"
+}
+
+function _help() {
+    cat <<EOF
+Script to make the creation of a Athena test area less painful.
+
+To use it you'll need:
+ - Make up you mind if you want to use the trunk or an older branch.
+   This is to be specified by an additional argument when sourcing
+   this script.
+
+This script will:
+ - Set up the required environment variables.
+ - Set up Athena
+EOF
+}
+
+# ________________________________________________________________________
 # random functions
 _files_exist () {
     files=$(shopt -s nullglob dotglob; echo *)
@@ -8,10 +31,15 @@ _files_exist () {
   return 1
     fi
 }
+if [ "$1" != "branch" -a "$1" != "trunk" ]; then
+    echo "ERROR: You did not decide on using either the branch or the trunk in your setup.\n"
+    _usage
+    _help
+    exit 1
+fi
 
 # setup atlas stuff
 # export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
-# asetup 20.1.6.3,AtlasDerivation,here,64
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
@@ -29,7 +57,7 @@ if (( $# < 1 )) ; then
    echo "Please enter the directory name (from current directory) in which you want to set up the test area: "
    read TestArea_name
 else
-    TestArea_name=$1
+    TestArea_name=$2
 fi
 echo "The test area will be set up in the directory: $PWD/$TestArea_name"
 
@@ -43,12 +71,24 @@ if _files_exist ; then
 fi
 
 # checkout packages
-asetup 20.1.6.3,AtlasDerivation,gcc48,here,64
-pkgco.py BTagging-00-07-43-branch
-pkgco.py JetTagTools-01-00-56-branch
-pkgco.py JetInterface-00-00-43
-pkgco.py JetMomentTools-00-03-20
-pkgco.py PileupReweighting-00-03-06
+if [[ "$1" == "branch" ]]; then
+    asetup 20.1.6.3,AtlasDerivation,gcc48,here,64
+    pkgco.py BTagging-00-07-43-branch
+    pkgco.py JetTagTools-01-00-56-branch
+    pkgco.py JetInterface-00-00-43
+    pkgco.py JetMomentTools-00-03-20
+    pkgco.py PileupReweighting-00-03-06
+elif [[ "$1" == "trunk" ]]; then
+    asetup 20.7.3.3,AtlasDerivation,gcc48,here,64
+    pkgco.py -A BTagging
+    pkgco.py -A JetTagTools
+    pkgco.py TrkVKalVrtFitter-00-07-08
+    pkgco.py InDetVKalVxInJetTool-00-06-07
+    pkgco.py VxSecVertex-00-04-07
+    pkgco.py JetInterface-00-00-43
+    pkgco.py JetMomentTools-00-03-20
+    pkgco.py PileupReweighting-00-03-06
+fi
 svn co svn+ssh://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/FlavourTagPerformanceFramework/trunk/xAODAthena xAODAthena
 setupWorkArea.py
 # build all the things
